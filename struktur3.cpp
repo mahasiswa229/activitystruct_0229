c7030e800a2cd18873595fe2e8d35ae75f57cec3
mahasiswa229 <161399711+mahasiswa229@users.noreply.github.com>
2024-04-02 15:44:22 +0800

    membuat input

--- struktur3.cpp
+++ struktur3.cpp
@@ -13,3 +13,20 @@ struct mahasiswa {
 	DetailAlamat alamat;
     int umur;
 };
+
+int main()
+{
+    mahasiswa mhs;
+	cout << "Nomor Mahasiswa = ";
+	getline(cin, mhs.nim);
+	cout << "Nama Mahasiswa = ";
+	getline(cin, mhs.nama);
+	cout << "Alamat Mahasiswa = " << endl;
+	cout << "\t nama Desa = ";
+	cin >> mhs.alamat.desa;
+	cout << "\t nama Kota = ";
+	cin >> mhs.alamat.kota;
+	cout << "Umur Mahsiswa = ";
+	cin >> mhs.umur;
+
+};
