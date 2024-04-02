f329720711f2f3585765fe74d2374a6bca3334a0
mahasiswa229 <161399711+mahasiswa229@users.noreply.github.com>
2024-04-02 15:29:32 +0800

    membuat output

--- struktur2.cpp
+++ struktur2.cpp
@@ -28,4 +28,12 @@ int main()
 	cin >> mhs.alamat.kota;
 	cout << "Umur Mahsiswa = ";
 	cin >> mhs.umur;
+
+    cout << endl;
+	cout << "\n NIM = " << mhs.nim;
+	cout << "\n Nama = " << mhs.nama;
+	cout << "\n Alamat = ";
+	cout << "\n Desa = " << mhs.alamat.desa;
+	cout << "\n Kota = " << mhs.alamat.kota;
+	cout << "\n Umur = " << mhs.umur;
 };
