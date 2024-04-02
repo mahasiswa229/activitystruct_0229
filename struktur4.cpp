f799853f248225a2b05e80fd182d0b054ee45a51
mahasiswa229 <161399711+mahasiswa229@users.noreply.github.com>
2024-04-02 15:54:29 +0800

    membuat output

--- struktur4.cpp
+++ struktur4.cpp
@@ -17,7 +17,8 @@ struct mahasiswa {
 int main()
 {
     mahasiswa mhs[3];
-	for (int i = 0; i < 3; i++) {
+	for (int i = 0; i < 3; i++) 
+    {
 		cout << "Data Mahasiswa Ke- " << (i + 1) << ":" << endl;
 		cout << "Nomor Mahasiswa = ";
 		getline(cin, mhs[i].nim);
@@ -32,5 +33,17 @@ int main()
 		cin >> mhs[i].umur;
 		cin.ignore(1, '\n');
 		cout << endl;
-        }
+    }
+        for (int i = 0; i < 3; i++) 
+    {
+		cout << endl;
+		cout << "Data Mahasiswa Ke- " << (i + 1) << ":" << endl;
+		cout << "\n NIM = " << mhs[i].nim;
+		cout << "\n Nama = " << mhs[i].nama;
+		cout << "\n Alamat = ";
+		cout << "\n Desa = " << mhs[i].alamat.desa;
+		cout << "\n Kota = " << mhs[i].alamat.kota;
+		cout << "\n Umur = " << mhs[i].umur;
+		cout << endl;
+	}
 }
